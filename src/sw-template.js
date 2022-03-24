86c32fef50ee8fe523e435cd48aaec8c9d95604b
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-03-24 14:54:54 -0600

    Finished pwa with workbox

---
+++ src/sw-template.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.loadModule('workbox-background-sync');
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+const cachekFirstNetwork = [
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+];
+
+registerRoute(
+  ({ url }) => {
+    return cachekFirstNetwork.includes(url.href);
+  },
+  new CacheFirst()
+);
+const cacheNetworkFirst = [
+  '/api/auth/renew',
+  '/api/events'
+];
+
+registerRoute(
+  ({ url }) => {
+    return cacheNetworkFirst.includes(url.pathname);
+  },
+  new NetworkFirst()
+);
+
+// Referencia
+// registerRoute(
+//   new RegExp('http://localhost:4000/api/auth/renew'),
+//   new NetworkFirst()
+// );
+
+// Posteos offline
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'PUT'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'DELETE'
+);
